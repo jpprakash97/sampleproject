823395d60847e1bfb34a8cebcca370bb55a81935
jayaprakash.a <jayaprakash.a@volantetech.com>
2022-12-28 18:18:42 +0530

    First commit

---
+++ src/test/java/scripts/Tc1.java
@@ -0,0 +1,11 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc1 {
+	@Test
+	public void demo() {
+		Reporter.log("Hello JAVA", true);
+	}
+}
---
+++ src/test/java/scripts/Tc4.java
@@ -0,0 +1,11 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc4 {
+	@Test
+	public void demo() {
+		Reporter.log("Hello Python", true);
+	}
+}
